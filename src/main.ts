4102621b109b2b16caa2175e2e79647d6b4353f1
Ibikunle Jamiu <ibidotun@gmail.com>
2024-07-25 22:12:36 +0100

    resolved anim

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
-import Navigation from "./navigation";
+class Navigation {
+  isOpen: boolean;
+
+  constructor() {
+    this.isOpen = true;
+  }
+
+  toggle = () => {
+    this.isOpen = !this.isOpen;
+    this.update();
+  };
+
+  update = () => {
+    const header = document.getElementById("header-text") as HTMLElement;
+    header.style.scale = this.isOpen ? "1" : "0";
+
+    const container = document.getElementById("container") as HTMLElement;
+    container.style.width = this.isOpen ? "16rem" : "75px";
+
+    const toggle = document.getElementById("toggle") as HTMLElement;
+    toggle.style.right = this.isOpen ? "0.75rem" : "1.75rem";
+  };
+
+  start = () => {
+    this.listeners();
+  };
+
+  listeners = () => {
+    window.addEventListener("DOMContentLoaded", () => {
+      this.update();
+    });
+  };
+}
 
 const navigation = new Navigation();
+navigation.start();
--- src/navigation.ts
+++
@@ -1,7 +0,0 @@
-export default class Navigation {
-  constructor() {}
-
-  start = () => {
-    // const app = document.getElementById("app");
-  };
-}
